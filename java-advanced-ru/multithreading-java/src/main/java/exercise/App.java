53982d06180afbb45f27ddd19235ee9c420278de
@hexlet/cli <support@hexlet.io>
2023-05-29 23:02:16 +0300

    submit java-advanced-ru/multithreading-java

--- java-advanced-ru/multithreading-java/src/main/java/exercise/App.java
+++ java-advanced-ru/multithreading-java/src/main/java/exercise/App.java
@@ -7,30 +7,33 @@ class App {
     private static final Logger LOGGER = Logger.getLogger("AppLogger");
 
     // BEGIN
-    static MaxThread threadMaxSearch;
-    static MinThread threadMinSearch;
-
     public static void main(String[] args) {
         int[] numbers = {10, -4, 67, 100, -100, 8};
         System.out.println(App.getMinMax(numbers));
     }
 
     public static Map<String, Integer> getMinMax(int[] arrayInt) {
-        threadMaxSearch = new MaxThread(arrayInt);
-        threadMinSearch = new MinThread(arrayInt);
+        MaxThread threadMaxSearch = new MaxThread(arrayInt);
+        MinThread threadMinSearch = new MinThread(arrayInt);
         threadMaxSearch.start();
         LOGGER.info("Thread " + threadMaxSearch.getName() + " started");
+        try {
+            threadMaxSearch.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        LOGGER.info("Thread " + threadMaxSearch.getName() + " finished");
         threadMinSearch.start();
         LOGGER.info("Thread " + threadMinSearch.getName() + " started");
-        int max = 0;
-        int min = 0;
-        if (threadMaxSearch.isInterrupted() && threadMinSearch.isInterrupted()) {
-            LOGGER.info("Thread " + threadMaxSearch.getName() + " finished");
-            LOGGER.info("Thread " + threadMinSearch.getName() + " finished");
-            max = threadMaxSearch.getResult();
-            min = threadMinSearch.getResult();
+        try {
+            threadMinSearch.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-        return Map.of("max", max, "min", min);
+        LOGGER.info("Thread " + threadMinSearch.getName() + " finished");
+        int max = threadMaxSearch.getResult();
+        int min = threadMinSearch.getResult();
+        return Map.of("min", min, "max", max);
     }
     // END
 }
